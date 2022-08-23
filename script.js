3be670b69d2a57979d4c4e2ff8c2ea34b93da55c
Mahesh Gaddam <mahesh.gaddam@peopletech.com>
2022-08-23 17:59:55 +0530

    Final Submit

--- script.js
+++ script.js
@@ -1,126 +1,52 @@
-function formValidation() {
-    var eid = document.registration.empid;
-    var fname = document.registration.firstname;
-    var lname = document.registration.lastname;
-    var empemail = document.registration.email;
-    var erole = document.registration.role;
-    var mstatus = document.registration.status;   
-    var msex = document.registration.male;
-    var fsex = document.registration.female;
-    var empsubmit = document.registration.submit;
-    if (empid(eid)) {
-        if (alphabet(fname)) {
-            if (alphabet(lname)) {
-                if (ValidateEmail(empemail)) {
-                    if (emprole(erole)) {
-                        if (maritalstatus(mstatus)) {
-                                if (validsex(msex,fsex)) {
-                                    if (sub(empsubmit)){
-
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-    return false;
-}
-
-
-
-function empid(eid) {
-    var eid_len = eid.value.length;
-    if (eid_len == 0) {
-        alert("Employee ID should not be empty");
-        eid.focus();
+function validate(callback) {
+    var id = document.registration.eid;
+    var fname = document.registration.fname;
+    var lname = document.registration.lname;
+    var email = document.registration.email;
+    var role = document.registration.role;
+    var gender = document.registration.gender;
+    var status = document.registration.status; 
+   
+    if (id.value.length <= 0) {
+        alert("Employee ID is required");
+        id.focus();
         return false;
     }
-    return true;
-}
-
-
-function alphabet(fname) {
-    var letters = /^[A-Za-z]+$/;
-    if (fname.value.match(letters)) {
-        return true;
-    }
-    else {
-        alert('First name should not be empty');
+    if (fname.value.length <= 0) {
+        alert("First Name is required");
         fname.focus();
         return false;
     }
-}
-
-function alphabet(lname) {
-    var letters = /^[A-Za-z]+$/;
-    if (lname.value.match(letters)) {
-        return true;
-    }
-    else {
-        alert('Last name should not be empty');
+    if (lname.value.length <= 0) {
+        alert("Last Name is required");
         lname.focus();
         return false;
     }
-}
-
-function ValidateEmail(empemail) {
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (empemail.value.match(mailformat)) {
-        return true;
-    }
-    else {
-        alert("Invalid email address");
-        empemail.focus();
+    if (email.value.length <= 0) {
+        alert("Email Id is required");
+        email.focus();
         return false;
     }
-}
-
-function emprole(role) {
-    var letters = /^[A-Za-z]+$/;
-    if (role.value.match(letters)) {
-        return true;
-    }
-    else {
-        alert('Role should not be empty');
+    if (role.value.length <= 0) {
+        alert("Role is required");
         role.focus();
         return false;
     }
-}
-
-function maritalstatus(mstatus) {
-    if (mstatus.value == "Default") {
-        alert('Select your marital status from the list');
-        mstatus.focus();
+    if (gender.value.length <= 0) {
+        alert("Gender is required");
+        gender.focus();
         return false;
-    }
-    else {
-        return true;
-    }
-}
-
-
-function validsex(msex,fsex) {
-    x = 0;
-
-    if (msex.checked) {
-        x++;
-    } if(fsex.checked) {
-        x++;
     }
     
-    if (x == 0) {
-        alert('Select Male/Female');
-        msex.focus();
+    if (status.value == "Default") {
+        alert("Marital status is required");
+        status.focus();
         return false;
     }
-    else {
-        console.log(eid);
-        window.location.reload()
-        return true;
-}
+    
+
+    callback();
+    return false;
 }
 
 
